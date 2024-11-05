911e4856aa98ef6c51749db9d6968194329835d6
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-11-05 22:10:13 +0000

    update

---
+++ scrape-flow/actions/workflows/createWorkflow.ts
@@ -0,0 +1,10 @@
+"use server"
+
+import { createWorkflowSchema, createWorkflowSchemaType } from "@/schema/workflow"
+import { z } from "zod"
+
+export async function CreateWorkFlow(
+    form: createWorkflowSchemaType
+) {
+
+}
--- scrape-flow/components/CustomDialogHeader.tsx
+++ scrape-flow/components/CustomDialogHeader.tsx
@@ -1,48 +1,54 @@
-"use client"
+"use client";
 
 import { LucideIcon } from "lucide-react";
-import { DialogHeader, DialogTitle } from "./ui/dialog"
+import { DialogHeader, DialogTitle } from "./ui/dialog";
 
-
-import React from 'react'
+import React from "react";
 import { cn } from "@/lib/utils";
+import { Separator } from "./ui/separator";
 
 type Props = {
-    title?: string;
-    subTitle?: string;
-    icon?:LucideIcon;
-    iconClassName?:string;
-    titleClassName?:string;
-    subTitleClassName?:string;
-}
+  title?: string;
+  subTitle?: string;
+  icon?: LucideIcon;
+  iconClassName?: string;
+  titleClassName?: string;
+  subTitleClassName?: string;
+};
 
 const CustomDialogHeader = (props: Props) => {
- 
   return (
-   <DialogHeader className="py-6">
-    <DialogTitle asChild>
+    <DialogHeader className="py-6">
+      <DialogTitle asChild>
         <div className="flex flex-col items-center gap-2 mb-2">
-{props.icon && <props.icon size={30} className={cn("stroke-primary", props.iconClassName
-)} />}
+          {props.icon && (
+            <props.icon
+              size={30}
+              className={cn("stroke-primary", props.iconClassName)}
+            />
+          )}
 
-{props.title && (
-    <p className={cn("tex-xl text-primary", props.titleClassName)}>
-{props.title}
-    </p>
-)}
+          {props.title && (
+            <p className={cn("tex-xl text-primary", props.titleClassName)}>
+              {props.title}
+            </p>
+          )}
 
-
-{props.subTitle && (
-    <p className={cn("tex-xl text-primary", props.subTitleClassName)}>
-{props.subTitle}
-    </p>
-)}
-    
+          {props.subTitle && (
+            <p
+              className={cn(
+                "tex-sm text-muted-foreground",
+                props.subTitleClassName
+              )}
+            >
+              {props.subTitle}
+            </p>
+          )}
         </div>
+      </DialogTitle>
+      <Separator />
+    </DialogHeader>
+  );
+};
 
-    </DialogTitle>
-   </DialogHeader>
-  )
-}
-
-export default CustomDialogHeader
+export default CustomDialogHeader;

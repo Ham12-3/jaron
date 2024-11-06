209da95350ed6205fafc5501b1f06f16c57e26db
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-11-06 06:35:43 +0000

    update

--- scrape-flow/actions/workflows/createWorkflow.ts
+++ scrape-flow/actions/workflows/createWorkflow.ts
@@ -1,10 +1,41 @@
 "use server"
 
+import prisma from "@/lib/prisma"
 import { createWorkflowSchema, createWorkflowSchemaType } from "@/schema/workflow"
-import { z } from "zod"
+import { WorkflowStatus } from "@/types/workflow"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
 
 export async function CreateWorkFlow(
     form: createWorkflowSchemaType
 ) {
+const {success, data} =createWorkflowSchema.safeParse(form)
+
+if(!success) {
+    throw new Error("Invalid form data")
+}
+const {userId} = auth()
+
+if(!userId) {
+    throw new Error("User not authenticated")
+}
+
+
+const result = await prisma.workflow.create({
+    data: {
+        userId,
+        status: WorkflowStatus.DRAFT,
+        definition: "TODO",
+        ...data
+    }
+})
+
+if(!result) {
+    throw new Error("Failed to create workflow")
+}
+
+
+redirect(`/workflow/editor/${result.id}`)
 
 }
--- scrape-flow/app/layout.tsx
+++ scrape-flow/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -40,6 +41,7 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
       </body>
+      <Toaster richColors/>
     </html>
     </ClerkProvider>
   );

e7054b852045d699c0b88588f7a79ff089c40ace
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-11-10 16:49:26 +0000

    update

--- scrape-flow/app/workflow/_components/TaskMenu.tsx
+++ scrape-flow/app/workflow/_components/TaskMenu.tsx
@@ -30,6 +30,8 @@ function TaskMenu() {
           </AccordionTrigger>
           <AccordionContent className="flex flex-col gap-1">
             <TaskMenuBtn taskType={TaskType.PAGE_TO_HTML} />
+            <TaskMenuBtn taskType={TaskType.EXTRACT_TEXT_FROM_ELEMENT} />
+            <TaskMenuBtn taskType={TaskType.LAUNCH_BROWSER} />
           </AccordionContent>
         </AccordionItem>
       </Accordion>
--- scrape-flow/lib/workflow/task/registry.tsx
+++ scrape-flow/lib/workflow/task/registry.tsx
@@ -1,9 +1,11 @@
+import { ExtractTextFromElementTask } from "./ExtractTextFromElement";
 import { LaunchBrowserTask } from "./LaunchBrowser";
 import { PagetoHtmlTask } from "./PageToHtml";
 
 export const TaskRegistry ={
     LAUNCH_BROWSER: LaunchBrowserTask,
-    PAGE_TO_HTML: PagetoHtmlTask
+    PAGE_TO_HTML: PagetoHtmlTask,
+    EXTRACT_TEXT_FROM_ELEMENT: ExtractTextFromElementTask,
 }
 
 

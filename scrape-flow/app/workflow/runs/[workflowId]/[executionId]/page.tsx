a4befc695d2d60c802ee1583a8a3fc87e8e4467c
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-11-13 04:12:58 +0000

    update

--- scrape-flow/app/workflow/runs/[workflowId]/[executionId]/page.tsx
+++ scrape-flow/app/workflow/runs/[workflowId]/[executionId]/page.tsx
@@ -1,3 +1,11 @@
+import { GetWorkflowExecutionWithPhases } from "@/actions/workflows/getWorkflowExecutionWIthPhases";
+import Topbar from "@/app/workflow/_components/topbar/Topbar";
+import { waitFor } from "@/lib/helper/waitFor";
+import { auth } from "@clerk/nextjs/server";
+import { Loader2Icon } from "lucide-react";
+
+import { Suspense } from "react";
+
 export default function ExecutionViewerPage({
   params,
 }: {
@@ -6,5 +14,40 @@ export default function ExecutionViewerPage({
     workflowId: string;
   };
 }) {
-  return <div className="flex flex-col h-screen w-full overflow-hidden">Run viewer</div>;
+  return <div className="flex flex-col h-screen w-full overflow-hidden">
+    <Topbar workflowId={params.workflowId} 
+    title="Workflow run details"
+    subTitle={`Run ID : ${params.executionId}`}
+    hideButtons
+    />
+
+    <section className="flex h-full overflow-auto">
+
+        <Suspense fallback={
+            <div className="flex w-full items-center justify-center">
+
+                <Loader2Icon className="h-10 w-10 animate-spin stroke-primary"/>
+            </div>
+        }>
+
+            <ExecutionViewerWrapper executionId = {params.executionId} />
+        </Suspense>
+    </section>
+  </div>;
+}
+
+
+async function ExecutionViewerWrapper({executionId}: {
+    executionId: string;
+}) {
+
+   
+
+    const workflowExecution = await GetWorkflowExecutionWithPhases(executionId)
+    if(!workflowExecution){
+        return <div>
+            Not Found
+        </div>
+    }
+    return <div>Wrapper</div>
 }

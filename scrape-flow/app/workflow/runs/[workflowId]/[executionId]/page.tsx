046d703035bd3e9a81314facc8d81c1d40501f56
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-11-13 09:03:22 +0000

    update

--- scrape-flow/app/workflow/runs/[workflowId]/[executionId]/page.tsx
+++ scrape-flow/app/workflow/runs/[workflowId]/[executionId]/page.tsx
@@ -5,6 +5,7 @@ import { auth } from "@clerk/nextjs/server";
 import { Loader2Icon } from "lucide-react";
 
 import { Suspense } from "react";
+import ExecutionViewer from "./_components/ExecutionViewer";
 
 export default function ExecutionViewerPage({
   params,
@@ -49,5 +50,5 @@ async function ExecutionViewerWrapper({executionId}: {
             Not Found
         </div>
     }
-    return <div>Wrapper</div>
+    return <ExecutionViewer initialData={workflowExecution} />
 }

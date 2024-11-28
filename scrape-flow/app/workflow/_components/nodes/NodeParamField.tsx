17e998c98f873cb98ea170d4637ba943e915cc70
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-11-28 14:10:40 +0000

    update

--- scrape-flow/app/workflow/_components/nodes/NodeParamField.tsx
+++ scrape-flow/app/workflow/_components/nodes/NodeParamField.tsx
@@ -7,42 +7,68 @@ import StringParam from "./param/StringParam";
 import { useReactFlow } from "@xyflow/react";
 import { AppNode } from "@/types/appNode";
 import BrowserInstanceParam from "./param/BrowserInstanceParam";
+import SelectParam from "./param/SelectParam";
 
-function NodeParamField({ param, nodeId, disabled }: { param: TaskParam; nodeId: string; disabled: boolean }) {
+function NodeParamField({
+  param,
+  nodeId,
+  disabled,
+}: {
+  param: TaskParam;
+  nodeId: string;
+  disabled: boolean;
+}) {
+  const { updateNodeData, getNode } = useReactFlow();
 
-    const {updateNodeData, getNode} = useReactFlow()
+  const node = getNode(nodeId) as AppNode;
+  const value = node?.data?.inputs[param.name];
 
-    const node = getNode(nodeId) as AppNode
-    const value = node?.data?.inputs[param.name]
-
-    const updateNodeParamValue = useCallback((newValue: string) => {
-updateNodeData(nodeId, {
-    inputs: {
-        ...node?.data.inputs,
-        [param.name]: newValue
-    }
-})
-    },[nodeId,updateNodeData,param.name,node?.data.inputs])
+  const updateNodeParamValue = useCallback(
+    (newValue: string) => {
+      updateNodeData(nodeId, {
+        inputs: {
+          ...node?.data.inputs,
+          [param.name]: newValue,
+        },
+      });
+    },
+    [nodeId, updateNodeData, param.name, node?.data.inputs]
+  );
 
   switch (param.type) {
     case TaskParamType.STRING:
-      return <StringParam param={param} value={value} updateNodeParamValue={updateNodeParamValue} disabled={disabled} />;
+      return (
+        <StringParam
+          param={param}
+          value={value}
+          updateNodeParamValue={updateNodeParamValue}
+          disabled={disabled}
+        />
+      );
     default:
       return (
         <div className="w-full">
           <p className="text-xs text-muted-foreground">Not implemented</p>
         </div>
       );
-      case TaskParamType.BROWSER_INSTANCE:
-        return (
-          <BrowserInstanceParam
+    case TaskParamType.BROWSER_INSTANCE:
+      return (
+        <BrowserInstanceParam
           param={param}
           value={""}
-        
           updateNodeParamValue={updateNodeParamValue}
-          />
-        )
+        />
+      );
 
+    case TaskParamType.SELECT:
+      return (
+        <SelectParam
+          param={param}
+          value={value}
+          updateNodeParamValue={updateNodeParamValue}
+          disabled={disabled}
+        />
+      );
   }
 }
 
